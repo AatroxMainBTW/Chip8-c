06ec4740ec6e91c6c390ae5a7ba9d0a39e009f72
AatroxMainBTW <fouzi.hammouda.pro@gmail.com>
2026-09-10 23:57:31 +0200

    [CHIP8] Fix first opcode 0x00E0, and key assignment WIP.

--- src/chip8.c
+++ src/chip8.c
@@ -6,7 +6,7 @@
 
 //region System
 void op_0x00E0(struct CHIP8* cpu){
-    memset(cpu->memory, 0, sizeof(cpu->memory));
+    memset(cpu->gfx, 0, sizeof(cpu->gfx));
     cpu->draw_flag = true;
 }
 
@@ -37,7 +37,63 @@ void op_0x1NNN(struct CHIP8* cpu, uint16_t opcode){
 }
 //endregion
 
+//region GFX
+void op_0xDXYN(struct CHIP8* cpu, uint16_t opcode) {
+    uint8_t X = cpu->V[(opcode & 0x0F00) >> 8];
+    uint8_t Y = cpu->V[(opcode & 0x00F0) >> 4];
+    //How much rows
+    uint8_t N = opcode & 0x000F;
+    //Collision
+    cpu->V[0xF] = 0;
+    for (int row = 0; row <= N-1; row++) {
+        uint8_t sprite_byte = cpu->memory[row + cpu->I];
 
+        for (int col = 0; col <= 7; col++) {
+            int sprite_bit = sprite_byte & (0x80 >> col);
+            if (sprite_bit != 0) {
+                int pos_x = (X + col) % 64;
+                int pos_y = (Y + row) % 32;
+                int index = pos_y * 64 + pos_x;
+
+                if (cpu->gfx[index] == 1) {
+                    cpu->V[0xF] = 1;
+                }
+                cpu->gfx[index] ^= 1;
+            }
+        }
+        cpu->draw_flag = true;
+    }
+}
+//endregion
+
+//region Conditional skips
+void op_0xEX9E(struct CHIP8* cpu, uint8_t key_index) {
+    if (cpu->keys[key_index] == 1) {
+        cpu->PC += 2;
+    }
+}
+void op_0xEXA1(struct CHIP8* cpu, uint8_t key_index) {
+    if (cpu->keys[key_index] == 0) {
+        cpu->PC += 2;
+    }
+}   
+//endregion
+
+//region Timers & key
+
+void op_0xFX0A(struct CHIP8* cpu, uint8_t x) {
+    bool key_pressed = false;
+
+    for (int key_index = 0; key_index < 16; key_index++) {
+        if (cpu->keys[key_index] == 1) {
+            cpu->V[x] = key_index;
+            key_pressed = true;
+            break;      
+        }
+    }
+    if (!key_pressed) cpu->PC -= 2;
+}
+//endregion
 
 void initialize_chip8(struct CHIP8* cpu){
     memset(cpu->gfx, 0, sizeof(cpu->gfx));
@@ -69,40 +125,92 @@ void emulate_cpu_cycle(struct CHIP8* cpu){
     uint16_t opcode = cpu->memory[cpu->PC] << 8 | cpu->memory[cpu->PC +1];
     cpu ->PC+=2;
     switch (opcode & 0xF000) {
-        case 0x0000:
-            switch (opcode & 0x00FF) {
-                case 0x00E0:
-                    op_0x00E0(cpu);
-                    //printf("System: 0x00E0 executed. \n");
-                    break;
-                case 0x00EE:
-                    op_0x00EE(cpu);
-                    //printf("System: 0x00EE executed. \n");
-                    break;
-                default:
-                    
-                    break;
-            }
-            printf("System: %02X%02X NOP\n", cpu->memory[cpu->PC], cpu->memory[cpu->PC +1]);
+    case 0x0000: {
+        switch (opcode & 0x00FF) {
+        case 0x00E0:
+            op_0x00E0(cpu);
+#ifdef DEBUG
+            printf("System: 0x00E0 executed. \n");
+#endif
             break;
-        case 0x1000:
-            op_0x1NNN(cpu, opcode);
-            printf("0x1NNN executed. \n");
-            break;
-        case 0x6000:
-            op_0x6XNN(cpu, opcode);
-            printf("0x6XNN executed. \n");
-            break;
-        case 0x7000:
-            op_0x7XNN(cpu, opcode);
-            printf("0x7XNN executed. \n");
-            break;
-        case 0xA000:
-            op_0xANNN(cpu, opcode);
-            printf("0xANNN executed. \n");
+        case 0x00EE:
+            op_0x00EE(cpu);
+
+#ifdef DEBUG
+            printf("System: 0x00EE executed. \n");
+#endif
             break;
         default:
-            printf("0xF000 NOP. \n");
+
+            break;
+        }
+        break;
+        }
+        case 0x1000:
+            op_0x1NNN(cpu, opcode);
+            
+#ifdef DEBUG
+            printf("0x1NNN executed. \n");
+#endif
+            break;
+        case 0x6000:
+            op_0x6XNN(cpu, opcode);          
+#ifdef DEBUG
+            printf("0x6XNN executed. \n");
+#endif
+            break;
+        case 0x7000:
+            op_0x7XNN(cpu, opcode);   
+#ifdef DEBUG
+            printf("0x7XNN executed. \n");
+#endif
+            break;
+        case 0xA000:
+            op_0xANNN(cpu, opcode);        
+#ifdef DEBUG
+            printf("0xANNN executed. \n");
+#endif
+            break;
+        case 0xD000:
+            op_0xDXYN(cpu, opcode);
+#ifdef DEBUG
+            printf("0xDXYN executed. \n");
+#endif
+            break;
+        case 0xE000: {
+            uint8_t key_index = cpu->V[(opcode & 0x0F00) >> 8];
+            switch (opcode & 0x00FF) {
+            case 0x9E:
+                op_0xEX9E(cpu, key_index);
+#ifdef DEBUG
+                printf("0xEX9E executed. \n");
+#endif
+                break;
+            case 0xA1:
+                op_0xEXA1(cpu, key_index);
+#ifdef DEBUG
+                printf("0xEXA1 executed. \n");
+#endif
+                break;
+            }
+            break;
+        }
+
+        case 0xF000: {
+            uint8_t X = (opcode & 0x0F00) >> 8;
+            switch (opcode & 0x00FF) {
+            case 0x0A:
+                op_0xFX0A(cpu, X);
+#ifdef DEBUG
+                printf("0xFX0A executed. \n");
+#endif
+
+                break;
+            }
+            break;
+        }
+        default:
+            printf("UNKNOWN opcode: 0x%04X\n", opcode);
             break;
     }
 }
--- src/main.c
+++ src/main.c
@@ -1,13 +1,88 @@
+#include <SDL2/SDL.h>
 #include "chip8.h"
 
 int main(int argc, char **argv){
     struct CHIP8 chip_8;
     printf("Starting..... \n");
-    initialize_chip8(&chip_8);
-    
-    load_rom("/Users/fouzi/C-Chip-8/test_roms/chip8-logo.ch8", &chip_8);
-    
-    while (true) {
-        emulate_cpu_cycle(&chip_8);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
     }
+
+    const SDL_Scancode keymap[16] = {
+     SDL_SCANCODE_X, SDL_SCANCODE_1, SDL_SCANCODE_2, SDL_SCANCODE_3,
+     SDL_SCANCODE_Q, SDL_SCANCODE_W, SDL_SCANCODE_E, SDL_SCANCODE_A,
+     SDL_SCANCODE_S, SDL_SCANCODE_D, SDL_SCANCODE_Z, SDL_SCANCODE_C,
+     SDL_SCANCODE_4, SDL_SCANCODE_R, SDL_SCANCODE_F, SDL_SCANCODE_V
+    };
+
+    SDL_Window* window = SDL_CreateWindow(
+        "CHIP-8",           // Window title
+        SDL_WINDOWPOS_CENTERED,          // X position
+        SDL_WINDOWPOS_CENTERED,          // Y position
+        800,                              // Width
+        600,                              // Height
+        SDL_WINDOW_SHOWN                  // Flags
+    );
+
+    if (!window) {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    int running = 1;
+    SDL_Event event;
+
+    initialize_chip8(&chip_8);
+
+    load_rom("C:/Users/Fouzi/Chip8-c/test_roms/6-keypad.ch8", &chip_8);
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 64, 32);  
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+            if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+                bool pressed = (event.type == SDL_KEYDOWN);
+                SDL_Scancode scancode = event.key.keysym.scancode;
+                for (int i = 0; i < 16; ++i) {
+                    if (scancode == keymap[i]) {
+                        chip_8.keys[i] = pressed;
+                        printf("key %X = %d\n", i, pressed); 
+                    }
+                }
+            }
+        }
+         
+
+        for (int i = 0; i < 10; i++) emulate_cpu_cycle(&chip_8);
+
+        uint32_t pixels[64 * 32];
+        for (int i = 0; i < 64 * 32; i++)
+            pixels[i] = chip_8.gfx[i] ? 0xFFFFFFFF : 0x000000FF;
+
+        SDL_UpdateTexture(texture, NULL, pixels, 64 * sizeof(uint32_t));
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);  // stretch 64x32 → whole window
+        SDL_RenderPresent(renderer);
+        
+        SDL_Delay(16);
+    }
+
+    // Cleanup
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+
 }
